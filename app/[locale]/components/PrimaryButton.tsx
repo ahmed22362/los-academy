b19344d43711873065e18fbd329766b4fb97d145
ahmedketaa <andf.750@gmail.com>
2023-11-22 17:46:36 +0200

    update error in previous pushing

--- app/[locale]/components/PrimaryButton.tsx
+++ app/[locale]/components/PrimaryButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-const PrimaryButton = ({text, ourStyle, onClick ,disabled}: {text: string,disabled:boolean , ourStyle: string, onClick?: () => void}) => {
+const PrimaryButton = ({text, ourStyle, onClick ,disabled}: {text: string,disabled?:boolean , ourStyle: string, onClick?: () => void}) => {
     return(
         <button disabled={disabled} className={ ourStyle} onClick={onClick}>{text}</button>
     )

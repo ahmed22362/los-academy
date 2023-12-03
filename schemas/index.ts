e92a141b99e8fe57a247efee97bb3de156d6fb17
ahmedmangood <ahmedmangood656@gmail.com>
2023-12-03 21:01:46 +0200

    add teacher schema and create validations errors

--- schemas/index.ts
+++ schemas/index.ts
@@ -42,4 +42,49 @@ const signupSchema: yup.Schema<SignUpUser> = yup.object().shape({
         .min(3, 'start from 3 years old')
         .max(20, 'not more than 20 years old'),
 });
-export default signupSchema;
+
+interface TeacherData {
+    name: string;
+    email: string;
+    phone: string;
+    password: string;
+    passwordConfirmation: string;
+    nationalId: string;
+    sessionCost: number;
+    role: string;
+}
+
+const TeacherSchema: yup.Schema<TeacherData> = yup.object().shape({
+    name: yup
+        .string()
+        .required('Required'),
+    email: yup
+        .string()
+        .required('Required')
+        .email('Not valid email'),
+    phone: yup
+        .string()
+        .required('Required'),
+    password: yup
+        .string()
+        .required('Required')
+        .matches(passwordRulez, 'make sure there is 6-40 characters at least 1 uppercase, 1 lowercase, 1 number and 1 special character'),
+    passwordConfirmation: yup
+        .string()
+        .required('Required')
+        .oneOf([yup.ref('password')], 'Not Match'),
+    nationalId: yup
+        .string()
+        .required('Required'),
+    sessionCost: yup
+        .number()
+        .min(1, 'must be 1 or more')
+        .required('Required'),
+    role: yup
+        .string()
+        .required('Required'),
+});
+
+
+
+export { signupSchema, TeacherSchema }

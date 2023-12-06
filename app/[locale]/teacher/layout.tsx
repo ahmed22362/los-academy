954794a090607b6c577809a8328bb28f07c78093
ahmedmangood <ahmedmangood656@gmail.com>
2023-12-07 00:44:32 +0200

    fix issue

--- app/[locale]/teacher/layout.tsx
+++ app/[locale]/teacher/layout.tsx
@@ -5,6 +5,7 @@ import "primereact/resources/primereact.min.css";
 import TeacherNavBar from "./components/teacherNavBar";
 import OurSideBar from "./components/ourSideBar";
 import { useEffect } from "react";
+// import io from 'socket.io-client';
 import Cookies from "universal-cookie";
 
 
@@ -15,7 +16,31 @@ export default function TeacherLayout(
     
     const cookies = new Cookies();
         
+    // useEffect(() => {
 
+    //         const socket = io(`https://bgjxgrgm-3000.uks1.devtunnels.ms/`, {
+    //             auth: {
+    //                 token: cookies.get('token')
+    //             },
+    //             autoConnect: true,
+    //             reconnection: false
+    //         })
+    
+    //         socket.on('connect', () => {
+    //             console.log('connect')
+    //             // socket.emit('hello')
+    //         })
+    
+    //         socket.on('sesionStarted', (data) => {
+    //             console.log('sesionStarted')
+    //             console.log(data)
+    //         })
+    
+    //         socket.on('disconnect', () => {
+    //         console.log('disconnect')
+    //         })
+
+    // }, [])
 
     return (
         <section>

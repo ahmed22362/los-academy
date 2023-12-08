99de5c12733791a92d5fa040258feedb3ca248f0
ahmedmangood <ahmedmangood656@gmail.com>
2023-12-08 19:37:28 +0200

    update responsive

--- app/[locale]/teacher/components/monthlyReport/monthlyReportCombBox.tsx
+++ app/[locale]/teacher/components/monthlyReport/monthlyReportCombBox.tsx
@@ -16,16 +16,16 @@ export default function MonthlyReportCombBox({...props}: any) {
 
     return(
         <section className={"px-5"}>
-            <div className={"flex flex-row w-full justify-between items-center bg-white-color p-5 rounded-[16px]"}>
+            <div className={"flex flex-row w-full justify-between items-center bg-white-color p-5 rounded-[16px] flex-wrap"}>
                 <form>
                     <input className={
-                        "border-0 rounded-[16px] w-[450px] focus:border-[2px] border-secondary-color transition-all"
+                        "border-0 rounded-[16px] w-[420px] max-md:w-full focus:border-[2px] border-secondary-color transition-all"
                     } type={"search"} placeholder={"search"} />
                 </form>
                 <button 
                     onClick={openModal}
                     className={
-                        "bg-white hover:bg-gray-100 transition-colors text-black-color-one px-5 py-2 rounded-[16px] font-normal"
+                        "bg-white hover:bg-gray-100 transition-colors text-black-color-one px-5 py-2 rounded-[16px] font-normal my-3"
                     }
                     >Add Report +</button>
             </div>

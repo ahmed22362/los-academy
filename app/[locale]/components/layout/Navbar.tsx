83f5973e5d1db9186488aa5d9809fac7650d715e
Ahmed <ahmedhamada496@gmail.com>
2025-07-14 00:01:43 +0300

    refactor(layout): clean up Navbar component and update text
    - Remove unused imports and simplify imports structure
    - Update "Learning Of Science" to "Language Of Science"
    - Standardize string quotes to single quotes
    - Remove redundant code and comments

--- app/[locale]/components/layout/Navbar.tsx
+++ app/[locale]/components/layout/Navbar.tsx
@@ -1,103 +1,96 @@
-"use client";
+'use client';
 
-import { useTranslations } from "next-intl";
-import LinkIntl from "next-intl/link";
-import Link from "next/link";
-import { MdLanguage } from "react-icons/md";
-import Image from "next/image";
-import { Dropdown, Navbar } from "flowbite-react";
-import type { CustomFlowbiteTheme } from "flowbite-react";
-import {
-  useParams,
-  usePathname,
-  useRouter,
-  useSearchParams,
-} from "next/navigation";
-import Cookies from "universal-cookie";
+import { useTranslations } from 'next-intl';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Navbar } from 'flowbite-react';
+import type { CustomFlowbiteTheme } from 'flowbite-react';
+import { usePathname, useRouter } from 'next/navigation';
+import Cookies from 'universal-cookie';
 
-import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
+import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
 
 export default function CustomNavbar() {
-  const UserDropDown = dynamic(() => import("./userDropDown"), {
+  const UserDropDown = dynamic(() => import('./userDropDown'), {
     ssr: false,
   });
 
-  const LoginButton = dynamic(() => import("./loginButton"), {
+  const LoginButton = dynamic(() => import('./loginButton'), {
     ssr: false,
   });
 
   const pathname = usePathname();
   const router = useRouter();
-  const isAdminDashboard = pathname.startsWith("/admin");
-  const isAdminLogin = pathname.startsWith("/los_auth");
-  const isteacher = pathname.startsWith("/teacher");
+  const isAdminDashboard = pathname.startsWith('/admin');
+  const isAdminLogin = pathname.startsWith('/los_auth');
+  const isteacher = pathname.startsWith('/teacher');
   const cookies = new Cookies();
-  const t = useTranslations("CustomNavbar");
+  const t = useTranslations('CustomNavbar');
 
-  const customNavTheme: CustomFlowbiteTheme["navbar"] = {
+  const customNavTheme: CustomFlowbiteTheme['navbar'] = {
     root: {
       inner: {
-        base: "flex flex-row justify-around max-lg:justify-between flex-wrap items-center px-2 max-md:px-5 max-sm:px-2 max-md:px-3",
+        base: 'flex flex-row justify-around max-lg:justify-between flex-wrap items-center px-2 max-md:px-5 max-sm:px-2 max-md:px-3',
         fluid: {
-          off: "",
-          on: "",
+          off: '',
+          on: '',
         },
       },
     },
     collapse: {
-      base: "w-full lg:block lg:w-auto rtl:xl:block",
-      list: "mt-4 flex flex-col lg:mt-0 lg:flex-row lg:space-x-8 lg:text-sm lg:font-medium gap-1 rtl:lg:space-x-3 rtl:gap-0",
+      base: 'w-full lg:block lg:w-auto rtl:xl:block',
+      list: 'mt-4 flex flex-col lg:mt-0 lg:flex-row lg:space-x-8 lg:text-sm lg:font-medium gap-1 rtl:lg:space-x-3 rtl:gap-0',
       hidden: {
-        on: "hidden",
-        off: "",
+        on: 'hidden',
+        off: '',
       },
     },
     toggle: {
-      base: "inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 lg:hidden rtl:xl:hidden rtl:mr-1",
-      icon: "h-6 w-6 shrink-0",
+      base: 'inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 lg:hidden rtl:xl:hidden rtl:mr-1',
+      icon: 'h-6 w-6 shrink-0',
     },
     link: {
       active: {
-        on: "text-secondary-color underline text-base rtl:font-semibold",
-        off: "text-base text-primary-color hover:text-secondary-color rtl:font-semibold hover:underline transition-colors",
+        on: 'text-secondary-color underline text-base rtl:font-semibold',
+        off: 'text-base text-primary-color hover:text-secondary-color rtl:font-semibold hover:underline transition-colors',
       },
     },
   };
 
-  const [userName, setUserName] = useState(cookies.get("name"));
+  const [userName, setUserName] = useState(cookies.get('name'));
 
   useEffect(() => {
     // This effect will run whenever the 'name' cookie changes
-    const newName = cookies.get("name");
+    const newName = cookies.get('name');
     setUserName(newName);
-  }, [cookies.get("name")]);
+  }, [cookies.get('name')]);
 
   const addActiveClass = (e: any) => {
-    const navLinks = document.querySelectorAll(".navBarLink");
+    const navLinks = document.querySelectorAll('.navBarLink');
     navLinks.forEach((link) => {
-      link.classList.remove("active");
+      link.classList.remove('active');
     });
-    e.target.classList.add("active");
+    e.target.classList.add('active');
   };
 
   const logOut = async () => {
-    const token = await cookies.get("token");
-    const id = await cookies.get("id");
-    const name = await cookies.get("name");
+    const token = await cookies.get('token');
+    const id = await cookies.get('id');
+    const name = await cookies.get('name');
 
     if (token && id && name) {
-      cookies.remove("token", { path: "/" });
-      cookies.remove("id", { path: "/" });
-      cookies.remove("name", { path: "/" });
+      cookies.remove('token', { path: '/' });
+      cookies.remove('id', { path: '/' });
+      cookies.remove('name', { path: '/' });
       if (!token && !id && !name) {
         // router.replace('/login');
-        window.location.href = "/login";
+        window.location.href = '/login';
       }
-      window.location.href = "/login";
+      window.location.href = '/login';
       // router.replace('/login');
     } else {
-      console.error("Error removing cookies");
+      console.error('Error removing cookies');
     }
   };
 
@@ -115,26 +108,26 @@ export default function CustomNavbar() {
               className="flex flex-row flex-wrap justify-center gap-3  rtl:flex-row-reverse"
             >
               <Image
-                src={"/logo.png"}
+                src={'/logo.png'}
                 alt="logo image"
                 width={30}
                 height={30}
                 priority={true}
-                loading={"eager"}
-                className={"w-auto h-auto max-md:w-[30px]"}
+                loading={'eager'}
+                className={'w-auto h-auto max-md:w-[30px]'}
               />
               <h2
-                className={"font-semibold "}
+                className={'font-semibold '}
                 style={{
                   fontSize:
-                    "calc(16px + (24 - 16) * ((100vw - 320px) / (1920 - 320))",
+                    'calc(16px + (24 - 16) * ((100vw - 320px) / (1920 - 320))',
                 }}
               >
                 LOS Academy
               </h2>
             </Link>
             <div className="text-[14px] ml-12 max-[450px]:text-[12px] font-semibold -translate-y-1 ">
-              Learning Of Science
+              Language Of Science
             </div>
           </div>
 
@@ -144,20 +137,20 @@ export default function CustomNavbar() {
           >
             <li>
               <Link
-                href={"/#home"}
+                href={'/#home'}
                 onClick={addActiveClass}
                 className={`navBarLink rtl:ml-5 active`}
               >
-                {t("home-link")}
+                {t('home-link')}
               </Link>
             </li>
             <li>
               <Link
-                href={"/#courses"}
+                href={'/#courses'}
                 onClick={addActiveClass}
                 className={`navBarLink rtl:ml-5 `}
               >
-                {t("courses-link")}
+                {t('courses-link')}
               </Link>
             </li>
             <li>
@@ -166,7 +159,7 @@ export default function CustomNavbar() {
                 onClick={addActiveClass}
                 className={` navBarLink rtl:ml-5 `}
               >
-                {t("about-link")}
+                {t('about-link')}
               </Link>
             </li>
             {/* <li>
@@ -184,7 +177,7 @@ export default function CustomNavbar() {
                 onClick={addActiveClass}
                 className={`navBarLink rtl:ml-5`}
               >
-                {t("feedback-link")}
+                {t('feedback-link')}
               </Link>
             </li>
             <li>
@@ -193,13 +186,13 @@ export default function CustomNavbar() {
                 onClick={addActiveClass}
                 className={`navBarLink rtl:ml-5`}
               >
-                {t("contact-link")}
+                {t('contact-link')}
               </Link>
             </li>
           </Navbar.Collapse>
           <div
             className={
-              "flex items-center max-md:items-baseline gap-2 rtl:font-sans rtl:text-lg rtl:max-sm:me-0 rtl:max-sm:ms-auto rtl:lg:w-[185px]"
+              'flex items-center max-md:items-baseline gap-2 rtl:font-sans rtl:text-lg rtl:max-sm:me-0 rtl:max-sm:ms-auto rtl:lg:w-[185px]'
             }
           >
             <Navbar.Toggle theme={customNavTheme.toggle} />
